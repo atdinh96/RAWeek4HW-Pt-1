12979f654b8a7fbb20f9375ff5ac8f1a5c3cfaed
atdinh96 <athenaleighdinh@gmail.com>
2022-12-31 11:29:25 -0800

    first commit

---
+++ JavaScript/index.js
@@ -0,0 +1,6 @@
+function greet(Name, Height, Country) 
+{
+    alert('Hello ' + Name + ' your height is '+ Height + ' and your country of origin is '+ Country);
+}
+
+greet('Athena','5\'4\"', 'USA');
